d6b54e692f4915791258e2b29c6f2a2b383141de
Chandima B Samarasinghe <chandi2398@gmail.com>
2016-05-09 11:43:33 +0530

    Update TX-F22_new_02_07-08-2015.ino

--- Remote_Controller_arduino_code/TX-F22_new_02_07-08-2015.ino
+++ Remote_Controller_arduino_code/TX-F22_new_02_07-08-2015.ino
@@ -8,10 +8,6 @@ fine calibrated , main wings angle increased , upperwing angle increased
   rUp(yellow)2             lUp(blue)11
   rBottom(green)4           lBottom(purple)12
 
-  
-successful code
-
-
 - modification 1 ,( no variables, functions) ,2MBPS 
 analog 14(a0) 15 16 17 18 19(a5)
 nano pwm 3,5,6,9,10,11 / normal digital 0,1,2,4,7,8,12,13
@@ -61,35 +57,13 @@ void setup() {
   pinMode(11 ,INPUT);
   pinMode(4 ,INPUT);
   pinMode(12 ,INPUT);
- /* pinMode( ,INPUT);
-  pinMode( ,INPUT);
-  pinMode( ,INPUT);
-  pinMode( ,INPUT); */
 
-  
   Serial.begin(9600);
   Serial.println("Transmitter Powered On");
   
   radio.begin(); //activate the modem
   radio.setDataRate(RF24_2MBPS);
   radio.setRetries(0,0);
-  
-  /*
-  radio.setChannel(1);
-  radio.setPALevel(RF24_PA_MAX);
-  radio.setDataRate(RF24_1MBPS);
-  radio.setAutoAck(1);                     // Ensure autoACK is enabled
-  radio.setRetries(2,15);                  // Optionally, increase the delay between retries & # of retries
-                                            It shows how many times the modem will retry to the send data in case of not receiving by another modem.
-                                            The first argument sets how often modem will retry. It is a multiple of 250 microseconds. 15 * 250 = 3750. So, if the recipient does not receive data, 
-                                            modem will try to send them every 3.75 milliseconds. Second argument is the number of attempts. So in our example,modem will try to send 15 times before
-                                            it will stop and finds that the receiver is out of range, or is turned off.
-  radio.setCRCLength(RF24_CRC_8);          // Use 8-bit CRC for performance
-  radio.openWritingPipe(pipes[0]);
-  radio.openReadingPipe(1,pipes[1]);
-  
-  radio.startListening(); //OR stopListening
-  */
   radio.openWritingPipe(addresses[1]); //sets the address of the receiver to which the program will send data.
   radio.openReadingPipe(1,addresses[0]); //channel(0-upto7), this device?
   
@@ -146,21 +120,10 @@ void loop() {
     TXDATA[2]=elivatorR - map(valJoyRX,neutJoyRX,0,0,p); // pwm servo, for joystick left-half  : mr fine working
     TXDATA[3]=elivatorL - map(valJoyRX,neutJoyRX,0,0,p); // pwm servo, for joystick left-half 
   }
- // Serial.println(" esc   ml    mr   tw   uw");
 
-  /*
-  Serial.print(TXDATA[0]);
-    Serial.print(" ");
-  Serial.print(TXDATA[1]);
-    Serial.print(" ");
-  Serial.print(TXDATA[2]);
-    Serial.print(" ");
-  Serial.println(TXDATA[3]);
- */
    
   radio.write(&TXDATA,sizeof(TXDATA));
-  //Serial.println("sent");
- //delay(100);
+
 }
 
 
@@ -184,14 +147,5 @@ void calibEsc(bool isAuto){
   }else{
    //manual calibration process. 
   }
-}
-
-/* read potentometers 
-
-4 potentometers included
-  LeftUp(brown)18            RightUp(orange)20
-  LeftBottom(purple)19        RightBottom(blue)21 */
-void readPotentometers(){
-  
 }
 
