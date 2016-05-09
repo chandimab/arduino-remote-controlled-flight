4b85aae1089e96a228a0bddfbf236e503f9d3ecb
Chandima B Samarasinghe <chandi2398@gmail.com>
2016-05-09 11:44:27 +0530

    Update RX-F22_new_02_07-08-2015.ino

--- Flight_arduino_code/RX-F22_new_02_07-08-2015.ino
+++ Flight_arduino_code/RX-F22_new_02_07-08-2015.ino
@@ -23,22 +23,6 @@ void setup() {
   
   radio.begin(); //activate the modem
   radio.setDataRate(RF24_2MBPS);
-  /*
-  radio.setChannel(1);
-  radio.setPALevel(RF24_PA_MAX);
-  radio.setDataRate(RF24_1MBPS);
-  radio.setAutoAck(1);                     // Ensure autoACK is enabled
-  radio.setRetries(2,15);                  // Optionally, increase the delay between retries & # of retries
-                                            It shows how many times the modem will retry to the send data in case of not receiving by another modem.
-                                            The first argument sets how often modem will retry. It is a multiple of 250 microseconds. 15 * 250 = 3750. So, if the recipient does not receive data, 
-                                            modem will try to send them every 3.75 milliseconds. Second argument is the number of attempts. So in our example,modem will try to send 15 times before
-                                            it will stop and finds that the receiver is out of range, or is turned off.
-  radio.setCRCLength(RF24_CRC_8);          // Use 8-bit CRC for performance
-  radio.openWritingPipe(pipes[0]);
-  radio.openReadingPipe(1,pipes[1]);
-  
-  radio.startListening(); //OR stopListening
-  */
   radio.openWritingPipe(addresses[0]); //sets the address of the receiver to which the program will send data.
   radio.openReadingPipe(1,addresses[1]); //channel(0-upto7), this device?
   
@@ -48,13 +32,10 @@ void setup() {
   serEsc.attach(5);
   serML.attach(14);
   serMR.attach(15);
-
-    
+  
   /* esc calibration */
  calibEsc(true); //auto calibration
-  
-  /*led */
-//  analogWrite(18,128); //A4  
+
 }
 
 void loop() {
@@ -62,23 +43,11 @@ void loop() {
     radio.read(&TXDATA, sizeof(TXDATA));
     
     if(TXDATA[0]=1993){ //noiseDetection CODE; geniune
-    /*  Serial.print(TXDATA[1]);
-          Serial.print(" ");
-        Serial.print(TXDATA[2]);
-          Serial.print(" ");
-        Serial.println(TXDATA[3]);/*
-          Serial.print(" ");
-        Serial.print(TXDATA[tw]);
-          Serial.print(" ");
-        Serial.println(TXDATA[uw]);
-        Serial.println(""); */
-      //Serial.println("genuine recieved");
       serEsc.writeMicroseconds(TXDATA[1]);
       serML.writeMicroseconds(TXDATA[2]);
       serMR.writeMicroseconds(TXDATA[3]);
     }else{ //noiseAvailable
         //writing previous value to controllers
-       // Serial.println("noise recieved");
       serEsc.writeMicroseconds(TXDATA[1]);
       serML.writeMicroseconds(TXDATA[2]);
       serMR.writeMicroseconds(TXDATA[3]);
